8b697587bc4b0696687b2663fc31ac01467588a1
Benoît Baliguet <benoit.baliguet@gmail.com>
2014-01-15 23:08:49 +0100

    Use express with Hogan

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var request = require("request"),
+
+	getTransportUrl = function(from, to) {
+		var url = "http://transport.opendata.ch/v1/connections?",
+			today = new Date(),
+			date = [today.getFullYear(), today.getMonth() + 1, today.getDate()].join("-"),
+			time = [today.getHours(), today.getMinutes()].join(":")
+			// build transport request 
+			url += [
+				"from=" + from,
+				"to=" + to,
+				"time=" + time,
+				"date=" + date
+			].join("&");
+
+		return url;
+	};
+
+/*
+ * GET home page.
+ */
+exports.index = function(req, res) {
+	request(getTransportUrl("Lausanne", "Morges"), function(error, response, body) {
+		var transports = JSON.parse(body);
+		res.render('index', {
+			title : "test", 
+			nbConnections: transports.connections.length
+		});
+	});
+};
