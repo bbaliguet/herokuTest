c0b5647a49a8e9bd4230027cf6737cffe5d18570
Benoît Baliguet <benoit.baliguet@gmail.com>
2014-01-26 20:43:09 +0100

    Add node unit

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,17 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       all: ['*.js', 'public/**/*.js', 'routes/**/*.js', 'views/**/*.js']
+    },
+    nodeunit: {
+      all: ['test/*.js']
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
 
 };
